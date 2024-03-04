bfcca086c40ddff8d46bfea19831f8d8718429d4
Quentin <quentinmet7@gmail.com>
2024-03-04 09:32:27 +0100

    Ajustement des urls pour les tests

--- project/src/test/api.test.js
+++ project/src/test/api.test.js
@@ -9,8 +9,8 @@ import {
   deleteProduct,
 } from "./fetch.js";
 
-const BASE_URL = "http://localhost:3000/api/products/";
-const NEW_URL = "http://localhost:3000/api/products/2";
+const BASE_URL = "http://localhost:3000/api/books/";
+const NEW_URL = "http://localhost:3000/api/books/2";
 
 test(`fetch the URL ${BASE_URL} with the metod GET `, async () => {
   expect(await getAllProducts(BASE_URL)).toStrictEqual(json);
@@ -20,11 +20,22 @@ test(`fetch the URL ${NEW_URL} with the method GET `, async () => {
   expect(await getOneProduct(NEW_URL)).toStrictEqual(newJson);
 });
 
+test(`fetch the URL ${NEW_URL} with the method DELETE`, async () => {
+  expect(await deleteProduct(NEW_URL));
+});
+
 test(`fetch the URL ${BASE_URL} with the method POST`, async () => {
   expect(
     await createProduct(BASE_URL, {
-      name: "Hello",
-      price: 5.99,
+      title: "Narutone n°1",
+      extrait:
+        "- Qu'est-ce que c'est... ... ce fameux serment ? - C'est de ne jamais dire qu'en réalité, tu es le démon-renard.",
+      year: new Date(2002, 10, 3, 1, 2, 3, 4),
+      editor: 1,
+      image:
+        "https://github.com/Timcodingeur/Projet-JS/blob/main/Image/Image-Books/Naruto1.jpg",
+      resume:
+        "Ce manga raconte l'histoire d'un jeune ninja du village de konoha qui s'appelle naruto uzumaki. Cet enfant est le pire élève de l'académie et n'est pas très aimé des villageois à cause d'un lourd secret qui le suit depuis sa naissance.",
       created: new Date(),
     })
   );
@@ -33,14 +44,17 @@ test(`fetch the URL ${BASE_URL} with the method POST`, async () => {
 test(`fetch the URL ${NEW_URL} with the method PUT`, async () => {
   expect(
     await updateProduct(NEW_URL, {
-      name: "hello",
-      price: 5.99,
+      title: "Naruto n°1",
+      extrait:
+        "- Qu'est-ce que c'est... ... ce fameux serment ? - C'est de ne jamais dire qu'en réalité, tu es le démon-renard.",
+      year: new Date(2002, 10, 3, 1, 2, 3, 4),
+      editor: 1,
+      image:
+        "https://github.com/Timcodingeur/Projet-JS/blob/main/Image/Image-Books/Naruto1.jpg",
+      resume:
+        "Ce manga raconte l'histoire d'un jeune ninja du village de konoha qui s'appelle naruto uzumaki. Cet enfant est le pire élève de l'académie et n'est pas très aimé des villageois à cause d'un lourd secret qui le suit depuis sa naissance.",
     })
   );
-});
-
-test(`fetch the URL ${NEW_URL} with the method DELETE`, async () => {
-  expect(await deleteProduct(NEW_URL));
 });
 
 test("Try a connection to the database", async () => {
--- project/src/test/datas.js
+++ project/src/test/datas.js
@@ -1,7 +1,10 @@
-const response = await fetch("http://localhost:3000/api/products/");
+const BASE_URL = "http://localhost:3000/api/products/";
+const NEW_URL = "http://localhost:3000/api/products/2";
+
+const response = await fetch(BASE_URL);
 const json = await response.json();
 
-const newResponse = await fetch("http://localhost:3000/api/products/2");
+const newResponse = await fetch(NEW_URL);
 const newJson = await newResponse.json();
 
 export { json, newJson };
